1fc143d213de90b0aa4a7d007f4feb540f024e12
haydenbock <hwb44@cornell.edu>
2025-04-03 11:38:08 -0400

    Publish v2

--- ColorToBW.R
+++ ColorToBW.R
@@ -7,6 +7,8 @@ library(magick)
 input_folder <- "~/Library/CloudStorage/OneDrive-ThePennsylvaniaStateUniversity(2)/PennState/GitRepo/RootImageConverter/RawImages"
 output_folder <- "~/Library/CloudStorage/OneDrive-ThePennsylvaniaStateUniversity(2)/PennState/GitRepo/RootImageConverter/BWImages"
 
+#remove placeholder text file in each RawImages/BWImages folder prior to running the code.
+
 # List all .tif files in the input folder
 # NOTE - change to .jpeg$ or other file name if not .tif images
 tif_files <- list.files(input_folder, pattern = "\\.tif$", full.names = TRUE)
